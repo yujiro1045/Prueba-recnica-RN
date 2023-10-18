326be05a5157f853fd00d71c9b759ec93a967448
Juan Medina <juanmedi730@gmail.com>
2023-10-18 10:04:32 -0500

    segundo commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import Main from "./src/screens/Main";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Main />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#1d1919",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ src/screens/Main.styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+const s = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+  textContainer: {
+    flex: 1,
+    borderWidth: 1,
+    justifyContent: "flex-start",
+  },
+  text1: {
+    color: "gray",
+    fontSize: 20,
+    marginTop: 50,
+  },
+  text2: {
+    color: "white",
+    fontSize: 25,
+  },
+});
+
+export default s;
